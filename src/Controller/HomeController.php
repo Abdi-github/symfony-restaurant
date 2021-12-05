033871b182778f216dd1e5d355d4a870c42337f4
abdigit <abdulkadirahmed1985@gmail.com>
2021-12-06 00:28:32 +0100

    QR Code and menu setup

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,7 +4,7 @@ namespace App\Controller;
 
 use App\Repository\CarouselRepository;
 use App\Repository\DishRepository;
-use App\Service\QrCodeService;
+// use App\Service\QrCodeService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,7 +12,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(CarouselRepository $cr, DishRepository $dr, QrCodeService $qrs, $baseUrl): Response
+    public function index(CarouselRepository $cr, DishRepository $dr): Response
     {
         // for ($query = 1; $query < 7; $query++) {
         //     $qrs->qrcode($query, $baseUrl);
