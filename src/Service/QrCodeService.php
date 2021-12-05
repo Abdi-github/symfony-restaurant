e318f726c614c78fb043d18d8ec4e1637a2cf323
abdigit <abdulkadirahmed1985@gmail.com>
2021-12-06 00:44:39 +0100

    QR Code and menu setup

--- src/Service/QrCodeService.php
+++ src/Service/QrCodeService.php
@@ -2,12 +2,12 @@
 
 namespace App\Service;
 
-// use Endroid\QrCode\Color\Color;
-// use Endroid\QrCode\Label\Margin\Margin;
-// use Endroid\QrCode\Encoding\Encoding;
-// use Endroid\QrCode\Builder\BuilderInterface;
-// use Endroid\QrCode\ErrorCorrectionLevel\ErrorCorrectionLevelHigh;
-// use Endroid\QrCode\Label\Alignment\LabelAlignmentCenter;
+use Endroid\QrCode\Color\Color;
+use Endroid\QrCode\Label\Margin\Margin;
+use Endroid\QrCode\Encoding\Encoding;
+use Endroid\QrCode\Builder\BuilderInterface;
+use Endroid\QrCode\ErrorCorrectionLevel\ErrorCorrectionLevelHigh;
+use Endroid\QrCode\Label\Alignment\LabelAlignmentCenter;
 
 class QrCodeService
 {
